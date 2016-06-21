67a31e694ec2bef3488028b6f3bdbc3ed82449f5
teguhedip <teguhediprasetyo@gmail.com>
2016-06-21 16:43:05 +0700

    init file

---
+++ app/src/main/java/com/oxmar/ww/firstmeet/LayoutingActivity.java
@@ -0,0 +1,14 @@
+package com.oxmar.ww.firstmeet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LayoutingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layouting);
+
+    }
+}
